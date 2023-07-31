035f146d0f1d76b7852124669b274bd21103cd3c
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-08-01 03:13:44 +0530

    corfur: Import msm_irqbalance.conf from holi

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_ODM_IMAGE := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS := all
 
