4b95e6a4dbd6dc47ec9e14e7ff64cc0d27338a44
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-12-08 20:50:01 +0530

    corfur: build fp vendor variant only and checkout manifest from stock

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     com.motorola.hardware.biometric.fingerprint@1.0.vendor
 
 # FM
