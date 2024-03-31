a46413b4452cc8411ada998d9d6ac3c9a4dc090e
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2024-03-31 00:30:40 +0000

    rhodep: Build libgui_shim_vendor
    Signed-off-by: Notkerd69 <29781314+Notkerd69@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
+    libgui_shim_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Dalvik
